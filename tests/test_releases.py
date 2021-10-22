5acd2918b64480fb7c73687cba922b8be6b1eda2
Adrian Tuzimek <tuziomek@gmail.com>
2021-10-22 20:07:17 +0200

    breaking change: mvp

---
+++ tests/test_releases.py
@@ -0,0 +1,75 @@
+import pytest
+from git_conventional_version.versioning.releases import FinalRelease
+from git_conventional_version.versioning.releases import ReleaseCandidateRelease
+
+
+def test_get_version_string_empty_repo(git_repo):
+    release_class = FinalRelease
+    release = release_class(git_repo)
+    old_version = release.get_old_version_string()
+    assert old_version == "0.0.0"
+    new_version = release.get_new_version_string()
+    assert new_version == "0.0.0"
+
+
+def test_get_version_string_tag_different_branch_repo(
+    git_repo_tag_different_branch
+):
+    release_class = FinalRelease
+    release = release_class(git_repo_tag_different_branch)
+    new_version = release.get_new_version_string()
+    assert new_version == "2.0.0"
+
+
+def test_get_version_string_nontagged_repo(git_repo_nontagged):
+    release_class = FinalRelease
+    release = release_class(git_repo_nontagged)
+    old_version = release.get_old_version_string()
+    assert old_version == "0.0.0"
+    new_version = release.get_new_version_string()
+    assert new_version == "0.1.0"
+
+
+def test_get_version_string_on_tagged_commit(git_repo_tagged_commit):
+    release_class = FinalRelease
+    release = release_class(git_repo_tagged_commit)
+    old_version = release.get_old_version_string()
+    assert old_version == "1.0.0"
+    new_version = release.get_new_version_string()
+    assert new_version == "1.0.0"
+
+
+@pytest.mark.parametrize(
+    "release_class,old_version_string,new_version_string",
+    [
+        (FinalRelease, "1.0.0", "1.1.0"),
+        (ReleaseCandidateRelease, "1.1.0rc0", "1.1.0rc1")
+    ]
+)
+def test_get_version_string_on_conventional_commit(
+    git_repo_conventional_commit,
+    release_class,
+    old_version_string,
+    new_version_string
+):
+    release = release_class(git_repo_conventional_commit)
+    old_version = release.get_old_version_string()
+    assert old_version == old_version_string
+    new_version = release.get_new_version_string()
+    assert new_version == new_version_string
+
+
+def test_get_prerelease_version_string(git_repo_prerelease):
+    release_class = ReleaseCandidateRelease
+    release = release_class(git_repo_prerelease)
+    old_version = release.get_old_version_string()
+    assert old_version == "1.1.0rc0"
+    new_version = release.get_new_version_string()
+    assert new_version == "1.1.0rc1"
+
+
+def test_get_next_prerelease_version_string(git_repo_next_prerelease):
+    release_class = ReleaseCandidateRelease
+    release = release_class(git_repo_next_prerelease)
+    new_version = release.get_new_version_string()
+    assert new_version == "1.1.0rc2"
---
+++ tests/test_versions.py
@@ -0,0 +1,28 @@
+import pytest
+
+from git_conventional_version.versioning.versions import FinalVersion
+from git_conventional_version.versioning.versions import ReleaseCandidateVersion
+from git_conventional_version.versioning.versions import DevelopmentalVersion
+
+
+@pytest.mark.parametrize(
+    "version_class,numbers,valid_version,invalid_version",
+    [
+        (FinalVersion, [1,2,3], "1.2.3", "1.2"),
+        (ReleaseCandidateVersion, [1,2,3,4], "1.2.3rc4", "1.2.3"),
+        (DevelopmentalVersion, [1,2,3,4], "1.2.3dev4", "1.2.3.4", )
+    ]
+)
+def test_version_classes(
+    version_class, 
+    numbers, 
+    valid_version, 
+    invalid_version
+):
+    version = version_class(numbers)
+    assert str(version) == valid_version
+    version_class.validate_tag(valid_version)
+    with pytest.raises(Exception):
+        version_class.validate_tag(invalid_version)
+    version = version_class.from_tag(valid_version)
+    assert str(version) == valid_version
